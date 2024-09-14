d879de3af97c327eb7d4f5dce0924a000903a989
Yash Tyagi <yashtyagi8006@gmail.com>
2024-09-14 12:51:44 +0530

    Blocking and non blocking requests

--- Hello-World/file.js
+++ Hello-World/file.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
+const os = require('os');
+
+console.log(os.cpus().length);
 
 
-// Sync..
+// Sync.. Blocking Request
 // fs.writeFileSync('./test.txt', "Hello World");
 
-// Async..
+// Async.. Non Blocking request
 // fs.writeFile('./test.txt', "Hello World Async", (err) => {});
 
 // const result = fs.readFileSync("./contact.txt", "utf-8");
@@ -19,13 +22,45 @@ const fs = require('fs');
 //     }
 // })
 
-fs.appendFileSync("./contact.txt", `${Date.now()} Hey There \n`);
+// fs.appendFileSync("./contact.txt", `${Date.now()} Hey There \n`);
 
-fs.cpSync('./test.txt', "./copy.txt");
+// fs.cpSync('./test.txt', "./copy.txt");
 
-fs.unlinkSync("./copy.txt");
+// fs.unlinkSync("./copy.txt");
 
-const result = fs.statSync('./test.txt');
+// const result = fs.statSync('./test.txt');
 // console.log(result);
 
-fs.mkdirSync("my-docs/a/b", {recursive : true});
+// fs.mkdirSync("my-docs/a/b", {recursive : true});
+
+// Blocking request 
+
+// console.log("1");
+
+// const result = fs.readFileSync("./contact.txt", "utf-8");
+// console.log(result);
+
+// console.log("2");
+// console.log("3");
+
+// Non Blocking Request
+
+// console.log("1");
+
+// fs.readFile("./contact.txt", "utf-8", (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(result);
+//     }
+// })
+
+
+// console.log("2");
+// console.log("3");
+
+// Default Thread Pool size = 4
+// Maz? - 8core cpu - 8
+
+// We should write a code that is non blocking
+
