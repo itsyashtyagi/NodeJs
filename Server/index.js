368f893c1f1823eb25c8eb730fd713ab09912bdb
Yash Tyagi <yashtyagi8006@gmail.com>
2024-09-14 16:59:49 +0530

    HTTP Methods

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,25 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     if(req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()} : ${req.url} New Request Recieved\n`;
+    const log = `${Date.now()} : ${req.method}  ${req.url} New Request Recieved\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
     
     fs.appendFile("log.txt", log, (err, data) => {
         switch (myUrl.pathname) {
             case "/":
-                res.end("HomePage");
+                if(req.method === 'GET') res.end("HomePage");
                 break;
             case "/about": 
                 const username = myUrl.query.myname;               
                 res.end(`Hi, ${username}`);
+                break;
+            case "/signup" : 
+                if(req.method === 'GET') res.end("This is a sign up form");
+                else if(req.method === 'POST') {
+                // DB Query
+                res.end("Success");
+                }
                 break;
             default :
                 res.end("404 Not Found");    
