3fffdfad0533ac3681261f816bec56ce274f1a11
Yash Tyagi <yashtyagi8006@gmail.com>
2024-09-15 12:03:01 +0530

    GET, PATCH, POST, DELETE api methods

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const users = require("./user_data.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8000;
+
+// Middleware - Plugin
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
 
 // Routes
 
@@ -21,10 +26,42 @@ app.route("/api/users/:id").get((req, res) => {
     return res.json(user);
 }).patch((req, res) => {
     // TODO : Edit the user with id
-    return res.json({status : "pending"});
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if(userIndex === -1){
+        res.json({status : "Failed to Edit the User Data"});
+    }
+
+    const updatedUser = {...users[userIndex], ...req.body};
+    users[userIndex] = updatedUser;
+
+    fs.writeFile("./user_data.json", JSON.stringify(users), (err, data) => {
+        if(err){
+            res.json({status : "Something went wrong"});
+        } else{
+            res.json({status : "User Detailed Updated Successfully", id : id});
+        }
+    })
+
 }).delete((req, res) => {
     // TODO : Delete the user with id
-    return res.json({status : "pending"});
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if(userIndex === -1){
+        return res.json({status : "Failed to Delete the user"});
+    }
+
+    users.splice(userIndex, 1);
+
+    fs.writeFile("./user_data.json", JSON.stringify(users), (err, data) => {
+        if(err){
+            return res.json({status : "Something went wrong"});
+        } else{
+            return res.json({status : "User Deleted Successfully", id : id});
+        }
+    });
 });
 
 //REST API
@@ -32,26 +69,34 @@ app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-});
-
 app.post("/api/users", (req, res) => {
-    // TODO : Create new user
-    return res.json({status : "pending"});
+    const body = req.body;
+    users.push({ id: users.length + 1, ...body});
+    fs.writeFile("./user_data.json", JSON.stringify(users), (err, data) => {
+        if(err){
+            return res.json({status : "something went wrong"});
+        }
+        else{
+            return res.json({status : "new user created"});
+        }
+    });
 });
 
-app.patch("api/users/:id", (req, res) => {
-    // TODO : Edit the user with id
-    return res.json({status : "pending"});
-});
+// app.get("/api/users/:id", (req, res) => {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// });
 
-app.delete("api/users/:id", (req, res) => {
-    // TODO : Delete the user with id
-    return res.json({status : "pending"});
-});
+// app.patch("api/users/:id", (req, res) => {
+//     // TODO : Edit the user with id
+//     return res.json({status : "pending"});
+// });
+
+// app.delete("api/users/:id", (req, res) => {
+//     // TODO : Delete the user with id
+//     return res.json({status : "pending"});
+// });
 
 
 app.listen(PORT, () => console.log(`Server Started at PORT ${PORT}`));
