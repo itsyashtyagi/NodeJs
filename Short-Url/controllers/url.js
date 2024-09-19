fd05e751c092bf3b76bade73200c7c5acaca0570
Yash Tyagi <yashtyagi8006@gmail.com>
2024-09-19 11:15:39 +0530

    Server side rendering with EJS

--- Short-Url/controllers/url.js
+++ Short-Url/controllers/url.js
@@ -11,7 +11,9 @@ async function handleGenerateNewShortUrl(req, res){
         visitHistory : [], 
     });
 
-    return res.json({ id : shortID});
+    return res.render("home", {
+        id : shortID,
+    });
 }
 
 async function handleGetAnalytics(req, res){
